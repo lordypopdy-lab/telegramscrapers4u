a30d645d0ee22742f1bfc4c9c9e70db713395e50
lordypopdy-lab <deelordpopdy2@gmail.com>
2025-04-19 19:00:29 -0700

    safe

--- Join.py
+++ Join.py
@@ -2,10 +2,10 @@ from telethon.sync import TelegramClient
 from telethon.tl.functions.channels import JoinChannelRequest
 
 # Replace these with your credentials
-phone_number = "+2348162131789"
-api_id = 24772601
-api_hash = "3132429a348ebdfb0cb6cea6f8100850"
-session_name = "new_session_21.session"  # This will store your session file
+phone_number = "+2349031415958"
+api_id = 22737247
+api_hash = "5f4a3178c89e34aefc9027c3f04a98be"
+session_name = "new_session_45"  # This will store your session file
 
 # Initialize Telethon client
 client = TelegramClient(session_name, api_id, api_hash)
@@ -19,7 +19,7 @@ async def join_group(group_link):
             print(f"Error: {e}")
 
 # Provide the group username or invite link
-group_link = "@BitclubChat2"  # Replace with actual group link
+group_link = "@BitclubCryptoNews"  # Replace with actual group link
 
 import asyncio
 asyncio.run(join_group(group_link))
